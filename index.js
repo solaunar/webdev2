b8c257cdce103c12c0ec4657b0eecbc2884cc916
Ana <anna.lesi2000@gmail.com>
2021-05-25 01:32:42 +0300

    added handlebars and style

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+
+const app = express();
+
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => res.render('home', {
+    title: 'Im the home page',
+}));
+
+const PORT = process.env.PORT || 9000
+
+app.listen(PORT, () =>{
+    console.log(`Listening on ${PORT}...`);
+});
