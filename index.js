bf363c41379646e5e5fe02dd161f7f50828057d3
Ana <anna.lesi2000@gmail.com>
2021-05-27 01:27:15 +0300

    added style

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
-
+const path = require("path");
 const app = express();
 
-app.engine('handlebars', handlebars());
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => res.render('home', {
-    title: 'Im the home page',
+    title: 'Book Search',
 }));
 
 const PORT = process.env.PORT || 9000
