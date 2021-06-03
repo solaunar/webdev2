e7da31e4f7d390db3d953aabf7b715f4ffcf2ce7
Ana <anna.lesi2000@gmail.com>
2021-06-04 01:59:06 +0300

    trial for save in db

--- index.js
+++ index.js
@@ -4,23 +4,42 @@ const path = require("path");
 const mongoose = require("mongoose");
 const app = express();
 const databaseURI = 'mongodb+srv://admin:webdev2project@webdev2db.bi4fs.mongodb.net/book-haven?retryWrites=true&w=majority';
-
+const FBook = require('./models/favorite-books');
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.render('home', {
+app.get('/', (req, res) => {
+    res.render('home', {
     title: 'Book Search',
-    script: 'search-books'
-}));
+    script: 'search-books'})
+    const book = new FBook({
+        workid: '0',
+        titleweb: 'mybook',
+        authorweb: 'myauthor',
+        series: 'myseries'
+    })
+    console.log(book);
+
+    book.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+});
 
 const PORT = process.env.PORT || 9000
 
 mongoose.connect(databaseURI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then((results)=> console.log('Connected to database book-haven.'),
-    app.listen(PORT, () =>{
-        console.log(`Listening on ${PORT}...`);
-    }))
+    .then((results)=> {
+        console.log('Connected to database book-haven.'),
+        app.listen(PORT, () =>{
+            console.log(`Listening on ${PORT}...`);
+        })
+    })
     .catch((err) => console.log(err));
+
