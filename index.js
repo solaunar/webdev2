0b29920689aa462b28b7213965854bf78a0b438e
Ana <anna.lesi2000@gmail.com>
2021-06-04 00:36:51 +0300

    first commit on mongoose and mongodb

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const path = require("path");
+const mongoose = require("mongoose");
 const app = express();
+const databaseURI = 'mongodb+srv://admin:webdev2project@webdev2db.bi4fs.mongodb.net/book-haven?retryWrites=true&w=majority';
+
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -15,6 +18,9 @@ app.get('/', (req, res) => res.render('home', {
 
 const PORT = process.env.PORT || 9000
 
-app.listen(PORT, () =>{
-    console.log(`Listening on ${PORT}...`);
-});
+mongoose.connect(databaseURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((results)=> console.log('Connected to database book-haven.'),
+    app.listen(PORT, () =>{
+        console.log(`Listening on ${PORT}...`);
+    }))
+    .catch((err) => console.log(err));
